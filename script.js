6c37c67ec173ab2d87f93714485615e16fd57b20
demandred1687 <149145786+demandred1687@users.noreply.github.com>
2024-01-04 20:46:57 +0800

    fixing css update

---
+++ script.js
@@ -0,0 +1,48 @@
+let num1 = '';
+let num2 = '';
+let op = '';
+
+const add = function(...args) {
+    let sum = 0;
+      for (let i = 0; i<args.length; i++) {
+    sum+=args[i];
+    }
+    return sum;
+  };
+  
+const subtract = function(x,y) {
+      x-=y;
+    return x;
+  };
+  
+const sum = function(arr) {
+    let sum = 0;
+      for (let i = 0; i<arr.length; i++) {
+    sum+=arr[i];
+    }
+    return sum;
+  };
+  
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; ++i) {
+   total *= arr[i];
+  }
+  return total;
+  };
+  
+const power = function(x,y) {
+      let total = x;
+    for (let i = y; i > 1; i--) {
+      total*=x;
+    }
+    return total;
+  };
+  
+const factorial = function(x) {
+  let total = x;
+  for (let i = x; i > 1; i--) {
+  total *= (i-1);
+  }
+  return total == 0 ? 1 : total;
+  };
